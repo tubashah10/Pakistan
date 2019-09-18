d5bd9cf611d4c339f7b8c17b79a1613c85b95ccd
Tuba Shah <tubashah@Tubas-MacBook-Air.local>
2019-09-18 12:12:08 +0800

    new website

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>All About Tuba!</h1>
+     <img width="500" src="https://image.winudf.com/v2/image/Y29tLmZsYWd3YWxscGFwZXIucGFraXN0YW5fc2NyZWVuXzBfMTUzMzQ1OTU3MF8wNjA/screen-0.jpg?fakeurl=1&type=.jpg" alt="a"
+     /> 
+
+    <p className="heading-2">Pakistan Cricket Team</p>
+
+     <img className="cricket-team" alt="a"width="500" src="https://english.cdn.zeenews.com/sites/default/files/2019/06/24/798312-pakistan-wc2.jpg"
+     />
+
+    <p className="heading-3">Capital City</p>
+     <img className="Capital"alt="a" width="500" src="https://dreamworldtravel.co.uk/blog/wp-content/uploads/2018/11/5-2.jpg"/>
+
+
+
+    </div>
+  );
+}
+
+export default App;
